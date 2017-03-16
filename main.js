1377ff21a01dbd1456797ed9c97ead14f3016e9d
JonCass <JCass1980@gmail.com>
2017-03-16 16:23:54 -0700

    adding stuff breaks it

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
+		var itemString =""
 
+		data.forEach(function(item){
+			itemString += `<div class="itemBox></div>`
+
+		})
+		$("#itemBlock").html(itemString)
+	})
 })
