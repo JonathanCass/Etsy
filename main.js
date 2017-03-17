b042ff80eaf5c167e3e4ed63bf31b429ee63e1a3
JonCass <JCass1980@gmail.com>
2017-03-17 12:55:07 -0700

    save before item box change

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
+	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=invader_zim&includes=Images,Shop&callback=?', function(data){
 		console.log(data)
 		var itemString =""
 
-		for( i = 0 ; i < 52 ; i++ ){
+		for( i = 0 ; i < 24 ; i++ ){
 			itemString += `<div id="itemBox${i}" class="itemBox"></div>`
 		}
 		$("#itemFence").html(itemString)
-		for( i = 0 ; i < 25 ; i++ ){
+		for( i = 0 ; i < 24 ; i++ ){
 			$(`#itemBox${i}`).css("background-image", `url(${data.results[i].Images[0].url_570xN})`)
 		}
 	})
