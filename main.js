3697930bc0e3d780415d13300b1b29fefc6ea2a3
JonCass <JCass1980@gmail.com>
2017-03-17 11:55:03 -0700

    Json object unusable

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
 		console.log(data)
-		console.log(data.length)
-
+		console.log(data[0].images)
 		var itemString =""
 
 		for( i = 0 ; i < 52 ; i++ ){
-			itemString += `<div class=itemBox></div>`
+			itemString += `<div id="itemBox${i}" class="itemBox"></div>`
 		}
 		$("#itemFence").html(itemString)
+		for( i = 0 ; i < 52 ; i++ ){
+			$(`#itemBox${i}`).css("background-image", "url(assets/cart.png)")
+		}
 	})
 })
