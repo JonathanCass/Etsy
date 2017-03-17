6112cadfe2bfa08730350ba9f3110f6ad08b7aa8
JonCass <JCass1980@gmail.com>
2017-03-17 10:13:33 -0700

    json objects unwieldly

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
+		console.log(data)
+		console.log(data.length)
+
 		var itemString =""
 
-		data.forEach(function(item){
-			itemString += `<div class="itemBox"></div>`
-
-		})
-		$("#itemBlock").html(itemString)
-		$("#itemBlock").html("ISTHISWORKINGATALL?")
+		for( i = 0 ; i < 52 ; i++ ){
+			itemString += `<div class=itemBox></div>`
+		}
+		$("#itemFence").html(itemString)
 	})
 })
