281389cab76972c893ee0f9157299f8251ae786a
JonCass <JCass1980@gmail.com>
2017-03-17 16:00:31 -0700

    hover hamburger in effect

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		var itemString =""
 
 		for( i = 0 ; i < 24 ; i++ ){
-			itemString += `<div class="itemBox"><div class = "image" id="imageActual${i}"><div class"hoverBar"><img src ="assets/heart.png" class="heart"><img src ="assets/hamburger.png" class="hamburger"></div></div><div class ="imageLabel" id="imageLabelActual${i}"><span class ="title" id="titleActual${i}" >TITLE</span><span class ="maker" id="makerActual${i}" >MAKER</span><span class ="price" id="priceActual${i}">PRICE</span></div></div>`
+			itemString += `<div class="itemBox"><div class = "image" id="imageActual${i}"><div class = "hoverBar"><img src ="assets/heart.png" class="heart"><img src ="assets/hamburger.png" class="hamburger"></div></div><div class ="imageLabel" id="imageLabelActual${i}"><span class ="title" id="titleActual${i}" >TITLE</span><span class ="maker" id="makerActual${i}" >MAKER</span><span class ="price" id="priceActual${i}">PRICE</span></div></div>`
 		}
 		$("#itemFence").html(itemString)
 		for( i = 0 ; i < 24 ; i++ ){
@@ -13,5 +13,11 @@ $(document).ready(function(){
 			$(`#makerActual${i}`).html(`${data.results[i].Shop.shop_name}`)
 			$(`#priceActual${i}`).html(`$${data.results[i].price}`)
 		}
+		$(".image").hover(function(){
+			$(this).children().addClass("showMe")
+			console.log(1)
+		}, function(){
+			$(this).children().removeClass("showMe")
+		})
 	})
 })
