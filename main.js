f0a71650506081a7ba84b1ebb592dd95930c7823
JonCass <JCass1980@gmail.com>
2017-03-17 12:37:48 -0700

    finally got pictures in the boxes

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
 	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
 		console.log(data)
-		console.log(data[0].images)
 		var itemString =""
 
 		for( i = 0 ; i < 52 ; i++ ){
 			itemString += `<div id="itemBox${i}" class="itemBox"></div>`
 		}
 		$("#itemFence").html(itemString)
-		for( i = 0 ; i < 52 ; i++ ){
-			$(`#itemBox${i}`).css("background-image", "url(assets/cart.png)")
+		for( i = 0 ; i < 25 ; i++ ){
+			$(`#itemBox${i}`).css("background-image", `url(${data.results[i].Images[0].url_570xN})`)
 		}
 	})
 })
+
+
+/*
+console.log(data.results[0].Images[0].url_570xN)
+
+
+
+
+
+
+*/
